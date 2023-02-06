0aa8e3defeccfce8a91ecbfea58771aab06d935a
minhthi <19522257@gm.uit.edu.vn>
2023-02-07 02:17:54 +0700

    update

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -11,7 +11,7 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-async function CreateNewAccessCode(phoneNumber) {
+ async function CreateNewAccessCode(phoneNumber) {
   const sixDigit = Math.floor(100000 + Math.random() * 900000)
   //save code to db with phone number
   await setDoc(doc(db, "phones", `${phoneNumber}`), {
@@ -26,7 +26,6 @@ async function CreateNewAccessCode(phoneNumber) {
 
 authRoute.post('/send-code', async (req, res) => {
   try {
-    const accessCode = req.body.code
     const phoneNumber = req.body.phoneNumber
     var msg
     //user did not submit a code
@@ -42,31 +41,33 @@ authRoute.post('/send-code', async (req, res) => {
 
 async function ValidateAccessCode(accessCode, phoneNumber) {
   var isValidate;
-  console.log(`validating ${phoneNumber}, ${accessCode}`)
-  //retrieve access code from database
+  //retrieve access code from db
   const querySnapshot = await getDocs(collection(db, "phones"));
   querySnapshot.forEach((doc) => {
     if(doc.id === phoneNumber ){
       console.log(doc.data().accessCode)
       if(doc.data().accessCode == accessCode ){
         isValidate = true;
+        
       }
     }
+ 
 });
-  if (isValidate) {
-    await setDoc(doc(db, "phones", `${phoneNumber}`), {
-      accessCode: '',
-    });
-  }
+if (isValidate) {
+  await setDoc(doc(db, "phones", `${phoneNumber}`), {
+    accessCode: '',
+  });
+}
   return isValidate
 }
 
-authRoute.post("/validate-code", async (req, res) => {
-    const accessCode = req.body.accessCode
-    const phoneNumber = req.body.phoneNumber
+authRoute.post("/validate", async (req, res) => {
     // check access code
-    await ValidateAccessCode(accessCode, phoneNumber)
-
-});
+    const isValidate = await ValidateAccessCode(req.body.accessCode, req.body.phoneNumber)
+    if (isValidate) {
+      return res.status(200).send();
+    }
+    return res.status(400).send();  }
+);
 
 export { authRoute };
